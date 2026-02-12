8f87fed6f4b0f2ac58d3bfa166207c1ffc46a828
Oberd Nougbognonhou Kouassi <kouassioberd39@gmail.com>
2026-02-12 11:49:18 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/codealpha_random_quote_generator/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.codealpha_random_quote_generator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.codealpha_random_quote_generator.ui.theme.CodeAlpha_Random_Quote_GeneratorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CodeAlpha_Random_Quote_GeneratorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CodeAlpha_Random_Quote_GeneratorTheme {
+        Greeting("Android")
+    }
+}
